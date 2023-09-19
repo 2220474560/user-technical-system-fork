4988adc5fb80c5c8c4cacd06c66a29e55f3447b8
waysx <1827288474@qq.com>
2023-09-19 20:42:00 +0800

    陈

--- src/main.js
+++ src/main.js
@@ -7,9 +7,10 @@ import './assets/css/global.css'
 import ElementUI from 'element-ui'
 // 引入ElementUI样式
 import 'element-ui/lib/theme-chalk/index.css'
-
+import axios from 'axios'
 Vue.use(ElementUI)
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
 
 new Vue({
   router,
--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -23,11 +23,11 @@
     </div>
     <!-- 注册 -->
     <div class="wz" v-else>
-      <el-input class="rounded-input" v-model="input" placeholder="请输入邮箱">
+      <el-input class="rounded-input" v-model="input1" placeholder="请输入邮箱">
       </el-input>
-      <el-input class="rounded-input" v-model="input" placeholder="请输入姓名">
+      <el-input class="rounded-input" v-model="input2" placeholder="请输入姓名">
       </el-input>
-      <el-input class="rounded-input" v-model="input" placeholder="请输入专业班级">
+      <el-input class="rounded-input" v-model="input3" placeholder="请输入专业班级">
       </el-input>
       <el-select class="selectStyle" v-model="value" placeholder="请选择">
     <el-option
@@ -37,9 +37,8 @@
       :value="item.value">
     </el-option>
   </el-select>
-  <el-input class="rounded-input" v-model="input" placeholder="请输入密码">
-      </el-input>
-      <el-input class="rounded-input" v-model="input" placeholder="请输入验证码">
+  <!-- 密码 -->
+  <el-input class="rounded-input" v-model="input5" placeholder="请输入密码">
       </el-input>
       <button class="rounded-button" type="primary" plain @click="login">注册</button>
       </div>
@@ -50,9 +49,12 @@
 </template>
 
 <script>
+import { Message } from 'element-ui'
 export default {
   data () {
     return {
+      // 注册的时候前端发给后端的东西
+      // 下拉框的数据传送
       options: [{
         value: '前端',
         label: '前端'
@@ -68,14 +70,62 @@ export default {
       }],
       value: '',
       radio: '1',
-      input: '',
+      input1: '',
+      input2: '',
+      input3: '',
+      input5: '',
       showLogin: true // 控制登录内容的显示
     }
   },
   methods: {
-    login () {
-      this.$router.push('/HomePage')
+    async login () {
+      const account = this.input1
+      const name = this.input2
+      const class1 = this.input3
+      const option = this.value
+      const password = this.input5
+      // 判断账号和密码是否为空
+      if (account.trim() === '' || password.trim() === '' || name.trim() === '' || option.trim() === '' || class1.trim() === '') {
+        // 如果账号或密码为空，则触发错误事件并给出相应提示
+        console.log('信息不能为空！')
+        Message.error('信息不能为空！！！')
+        return
+      }
+      if (!this.vaildFn()) {
+        this.$emit('error', '校验失败！')
+        console.log('当前邮箱号不合法！！')
+        return
+      }
+      this.$emit('success', '校验成功！')
+      console.log('当前邮箱号合法！！')
+      Message.success('注册成功！！！！！！')
+      this.input1 = ''
+      this.input2 = ''
+      this.input3 = ''
+      this.input5 = ''
+      // this.$router.push('/HomePage')不应该跳转到写一个页面了
+      const { data: res } = await this.$http.post('http://dev-cn.your-api-server.com/api/register',
+        {
+          email: this.input1,
+          username: this.input2,
+          class: this.input3,
+          position: this.value,
+          password: this.input5
+        })
+      console.log(res)
+      try {
+        console.log(res)
+      } catch (error) {
+        console.error(error)
+      }
     },
+    vaildFn () {
+      if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(this.input1)) {
+        return false
+      }
+      return true
+    },
+
     toggleContent () {
       // 点击注册按钮时切换显示的内容
       this.showLogin = !this.showLogin
@@ -85,6 +135,7 @@ export default {
     // }
   }
 }
+
 </script>
 
 <style scoped>
