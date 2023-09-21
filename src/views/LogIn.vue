b33ea726837c706ead5299476afebdb13e4391e2
unknown <1667752910@qq.com>
2023-09-21 11:39:05 +0800

    完成

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -12,14 +12,15 @@
       <h1 class="title">信 息 分 享 中 心</h1>
     </div>
     <div class="radio1">
-      <el-radio v-model="radio" label="1" @change="toggleContent">登录</el-radio>
+      <el-radio v-model="radio" label="1" @change="toggleContent" >登录</el-radio>
       <el-radio v-model="radio" label="2" @change="toggleContent">注册</el-radio>
     </div>
     <!-- 登录 -->
     <div class="wz" v-if="showLogin">
-      <el-input class="rounded-input" style="margin-bottom: 15%;" v-model="input" placeholder="请输入邮箱" ></el-input>
-      <el-input class="rounded-input" v-model="input" style="margin-bottom: 20%;" placeholder="请输入密码" ></el-input>
-      <button class="rounded-button" type="primary" plain @click="login">登录</button>
+      <el-input class="rounded-input" style="margin-bottom: 15%;" v-model="email"  placeholder="请输入邮箱" ></el-input>
+      <el-input class="rounded-input" v-model="password" style="margin-bottom: 20%;" placeholder="请输入密码" ></el-input>
+      <button class="rounded-button" type="primary" plain @click="getPostData">登录</button>
+      <p v-if="loginError" class="error-message">{{ loginErrorMessage }}</p>
     </div>
     <!-- 注册 -->
     <div class="wz" v-else>
@@ -53,6 +54,8 @@
 export default {
   data () {
     return {
+      loginError: false, // 是否显示错误提示
+      loginErrorMessage: '', // 登录错误提示消息
       options: [{
         value: '前端',
         label: '前端'
@@ -69,13 +72,27 @@ export default {
       value: '',
       radio: '1',
       input: '',
-      showLogin: true // 控制登录内容的显示
+      showLogin: true,
+      password: '',
+      email: ''
     }
   },
   methods: {
-    login () {
-      this.$router.push('/HomePage')
+    async getPostData () {
+      const { data: res } = await this.$axios.post('http://150.158.53.178:6290/api/login', {
+        email: this.email,
+        password: this.password
+      })
+      console.log(res)
     },
+    async login () {
+      // 添加验证逻辑
+      if (this.email === '' || this.password === '') {
+        this.loginError = true
+        this.loginErrorMessage = '邮箱和密码不能为空'
+      }
+    },
+    // 发送登录请求
     toggleContent () {
       // 点击注册按钮时切换显示的内容
       this.showLogin = !this.showLogin
